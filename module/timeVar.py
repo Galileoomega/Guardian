e6c715cdb89b299f9169a1a4464da5fcb7550b9a
galileoomega <mr.alex.woolruch@icloud.com>
2020-04-14 14:32:06 +0200

    Optimisation

--- module/timeVar.py
+++ module/timeVar.py
@@ -6,7 +6,8 @@ clickState = ""
 
 # Return the name of the file who has been clicked
 fileName = ""
-
+focusOnUsernameBar = False
+focusOnPasswordBar = False
 double_click_duration = 70
 fileType = False
 myPath = ""
