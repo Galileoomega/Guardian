327368d97f49614773f0c9c177245c0d010d0df4
galileoomega <mr.alex.woolruch@icloud.com>
2020-05-21 18:12:07 +0200

    Can now encrypt files

--- module/idVector.py
+++ module/idVector.py
@@ -3,6 +3,7 @@ from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 from module import cryptography
+from cryptography.fernet import Fernet
 
 # GET current path
 THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
@@ -72,4 +73,38 @@ def confirmationLogin(userPassword, iPressedMyLoginButton, userUsername):
 
         return loginIsOk, wrongPass
     
-def encryptFiles(listOfPaths):
+def convertPassword(userPassword):
+
+    passwordProvided = str(userPassword)
+    password = passwordProvided.encode()
+    salt = b'\x8a\x874\xe1\xb8+\x84O?\xbfR\xc9\x98\xa5L\xac'
+    kdf = PBKDF2HMAC(
+        algorithm=hashes.SHA256(),
+         length=32,
+        salt=salt,
+        iterations=100000,
+        backend=default_backend()
+    )
+    key = base64.urlsafe_b64encode(kdf.derive(password)) # Can only use kdf once
+    key = key.decode()
+    
+    return key
+
+def encryptFiles(listOfPaths, userPassword):
+    key = convertPassword(userPassword)
+    
+    for path in listOfPaths:
+
+        outputFile = path
+        outputFile += ".sha"
+
+        with open(path, 'rb') as f:
+            data = f.read()
+        
+        fernet = Fernet(key)
+        encrypted = fernet.encrypt(data)
+
+        with open(outputFile, 'wb') as f:
+            f.write(encrypted)
+        
+        os.remove(path)
